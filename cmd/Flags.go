32af23195032b2b5c22e98f133d346f1b488a218
DividedMoon <yidian20000105@163.com>
2022-08-19 23:56:15 +0800

    feat(flag): refine the const value

--- cmd/Flags.go
+++ cmd/Flags.go
@@ -11,6 +11,12 @@ type Flag struct {
 	cmdUsage string
 }
 
+const (
+	Src             = "src"
+	Des             = "des"
+	DefaultFilePath = ""
+)
+
 var (
 	cmd         *Flag
 	subcommands map[string]*Flag
@@ -21,34 +27,34 @@ func init() {
 		FlagSet:  flag.NewFlagSet("get", flag.ExitOnError),
 		cmdUsage: "Get the remote file(src) and download to local file(des).",
 	}
-	getCmd.String("src", "", "(required) the remote file on chunk server.")
-	getCmd.String("des", "./out.txt", "(required) the local file.")
+	getCmd.String(Src, DefaultFilePath, "(required) the remote file on chunk server.")
+	getCmd.String(Des, "./out.txt", "(required) the local file.")
 
 	addCmd := &Flag{
 		FlagSet:  flag.NewFlagSet("add", flag.ExitOnError),
 		cmdUsage: "Put the local file(src) and upload to remote file(des).",
 	}
-	addCmd.String("src", "", "(required) the remote file on chunk server.")
-	addCmd.String("des", "", "(required) the local file.")
+	addCmd.String(Src, DefaultFilePath, "(required) the remote file on chunk server.")
+	addCmd.String(Des, DefaultFilePath, "(required) the local file.")
 
 	removeCmd := &Flag{
 		FlagSet:  flag.NewFlagSet("remove", flag.ExitOnError),
 		cmdUsage: "Remove the remote file(des).",
 	}
-	removeCmd.String("des", "", "(required) the remote file.")
+	removeCmd.String(Des, DefaultFilePath, "(required) the remote file.")
 
 	moveCmd := &Flag{
 		FlagSet:  flag.NewFlagSet("move", flag.ExitOnError),
 		cmdUsage: "Move the remote file(src) to another remote file(des).",
 	}
-	moveCmd.String("src", "", "(required) the remote file on chunk server.")
-	moveCmd.String("des", "", "(required) the remote file that src moved to.")
+	moveCmd.String(Src, DefaultFilePath, "(required) the remote file on chunk server.")
+	moveCmd.String(Des, DefaultFilePath, "(required) the remote file that src moved to.")
 
 	listCmd := &Flag{
 		FlagSet:  flag.NewFlagSet("list", flag.ExitOnError),
 		cmdUsage: "List the all files in the remote Directory(des).",
 	}
-	listCmd.String("des", "", "(required) the remote Directory.")
+	listCmd.String(Des, DefaultFilePath, "(required) the remote Directory.")
 
 	// 注册
 	subcommands = map[string]*Flag{
